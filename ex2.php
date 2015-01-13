29f5da5403138ebaeeb4ed8d9c0d7317eec5cfbd
dandiesta <d.diesta@klab.com>
2015-01-13 11:56:24 +0800

    Exercise 2: Create and Use Variables

---
+++ ex2.php
@@ -0,0 +1,23 @@
+<html>
+	<head><title>Exercise #2</title></head>
+	<body>
+		<?php
+			echo '// Normal echo. <br>';
+			echo "Twinkle, Twinkle little star.<br>";
+
+			$twinkle = "Twinkle";
+			$star = "star";
+
+			echo ' <br>// Echo with variables. <br>';
+			echo "$twinkle, $twinkle little $star.<br>";
+
+			$twinkle = "baby";
+			$star = "pig";
+
+			echo ' <br>// Values of variables are changed. <br>';
+			echo "$twinkle, $twinkle little $star.<br>";
+
+		
+		?>
+	</body>
+</html>
