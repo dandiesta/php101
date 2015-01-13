a3e000c677d734f5aa2c19250c9731f731028d91
dandiesta <d.diesta@klab.com>
2015-01-13 12:13:25 +0800

    Exercise 3: Arithmetic Operators and Variables

---
+++ ex3.php
@@ -0,0 +1,24 @@
+<html>
+	<head><title>Exercise #3</title></head>
+	<body>
+		<?php
+			$x = 10;
+			$y = 7;
+   
+			$result=$x+$y;
+			echo "$x + $y = $result<br />";  
+			 
+			$result=$x-$y;
+			echo "$x - $y = $result<br />";  
+			 
+			$result=$x*$y;
+			echo "$x * $y = $result<br />";  
+			 
+			$result=$x/$y;
+			echo "$x / $y = $result<br />";  
+			 
+			$result=$x%$y;
+			echo "$x % $y = $result<br />"; 
+		?>
+	</body>
+</html>
