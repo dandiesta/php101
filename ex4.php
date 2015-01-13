1617e4bd52d1b8e11149d3708fd10dd8611ea136
dandiesta <d.diesta@klab.com>
2015-01-13 12:24:48 +0800

    Exercise 4: Arithmetic-Assignment Operators and Variables

---
+++ ex4.php
@@ -0,0 +1,29 @@
+<html>
+	<head><title>Exercise #4</title></head>
+	<body>
+		<?php
+			$string = "Value is now ";
+			$value = 8;
+			echo "$string $value.<br>";
+
+			$value += 2;
+			echo "Add 2. $string $value.<br>";
+
+			$value -=4;
+			echo "Subtract 4. $string $value.<br>";
+
+			$value *=5;
+			echo "Multiply by 5. $string $value.<br>";
+
+			$value /=3;
+			echo "Divide by 3. $string $value.<br>";
+
+			$value++;
+			echo "Increment value by one. $string $value.<br>";
+
+			$value--;
+			echo "Decrement value by one. $string $value.<br>";
+
+		?>
+	</body>
+</html>
