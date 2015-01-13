5bb0baf2c553909ea71a84c21e5d4bbb128988b6
dandiesta <d.diesta@klab.com>
2015-01-13 14:47:55 +0800

    Display string, values within a table

---
+++ w3/ex13.php
@@ -0,0 +1,27 @@
+<?php
+
+$A = 1000;
+$B = 1200;
+$C = 1400;
+
+echo "<table border=1>
+	<tr>
+		<td>
+			<font color=blue>Salary of Mr. A is</font>
+		</td>
+		<td>$A\$</td>
+	</tr>
+	<tr>
+		<td>
+			<font color=blue>Salary of Mr. B is</font>
+		</td>
+		<td>$B\$</td>
+	</tr>
+	<tr>
+		<td>
+			<font color=blue>Salary of Mr. C is</font>
+		</td>
+		<td>$C\$</td>
+	</tr>
+	"
+?>
