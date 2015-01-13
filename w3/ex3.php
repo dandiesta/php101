e0c021cca6f56277ffd188cbd525c48737a4671a
dandiesta <d.diesta@klab.com>
2015-01-13 14:01:28 +0800

    Put variable into the title section, h3 tag and as an anchor text

---
+++ w3/ex3.php
@@ -0,0 +1,19 @@
+<?php
+
+	$var = "PHP Tutorial";
+?>
+	<html>
+		<head>
+			<title><?php  echo $var; ?></title>
+		</head>
+		<body>
+			<h3><?php echo $var; ?></h3>
+			<p>PHP, an acronym for Hypertext Preprocessor, 
+				is a widely-used open source general-purpose scripting language. 
+				It is a cross-platform, HTML embedded server-side scripting 
+				language and is especially suited for web development.</p>
+
+			<p><a href="http://www.w3resource.com/php/php-home.php">
+				Go to the <?php echo $var; ?></a>.</p> 
+		</body>
+	</html>
