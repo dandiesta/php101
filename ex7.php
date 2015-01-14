ed11d11b7def06affc78072e4fb361c9aa8d7286
dandiesta <d.diesta@klab.com>
2015-01-14 16:52:30 +0800

    Variable Data Types

---
+++ ex7.php
@@ -0,0 +1,16 @@
+<?php
+$whatsit = "Dan";
+echo "Value is " . gettype($whatsit) . ".<br>";
+
+$whatsit = 17.23;
+echo "Value is " . gettype($whatsit) . ".<br>";
+
+$whatsit = true;
+echo "Value is " . gettype($whatsit) . ".<br>";
+
+$whatsit = 25;
+echo "Value is " . gettype($whatsit) . ".<br>";
+
+$whatsit = null;
+echo "Value is " . gettype($whatsit) . ".<br>";
+?>
